87eb64ef7ffa733b892988ce2342d4201b1e5761
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-24 21:21:20 +0300

    feat(002): Реализовал class HibernateLocationCrudDAO

--- src/main/java/ru/dragomirov/dao/HibernateLocationCrudDAO.java
+++ src/main/java/ru/dragomirov/dao/HibernateLocationCrudDAO.java
@@ -1,4 +1,41 @@
 package ru.dragomirov.dao;
 
-public class HibernateLocationCrudDAO {
+import ru.dragomirov.entities.Location;
+import ru.dragomirov.utils.HibernateSessionManagerUtil;
+
+import java.util.List;
+import java.util.Optional;
+
+public class HibernateLocationCrudDAO implements CrudDAO<Location, Integer>{
+    @Override
+    public void create(Location entity) {
+        HibernateSessionManagerUtil.performTransaction(session ->
+                session.save(entity));
+    }
+
+    @Override
+    public List<Location> findAll() {
+        return HibernateSessionManagerUtil.performSessionQuery(session ->
+                session.createQuery("FROM Location", Location.class).list(),
+                "Произошла ошибка при выполнении метода 'findAll'(HibernateLocationCrudDAO)");
+    }
+
+    @Override
+    public Optional<Location> findById(Integer id) {
+        return Optional.ofNullable(HibernateSessionManagerUtil.performSessionQuery(session ->
+                session.get(Location.class, id),
+                "Произошла ошибка при выполнении метода 'findById'(HibernateLocationCrudDAO)"));
+    }
+
+    @Override
+    public void update(Location entity) {
+        HibernateSessionManagerUtil.performTransaction(session ->
+                session.update(entity));
+    }
+
+    @Override
+    public void delete(Integer id) {
+        HibernateSessionManagerUtil.performTransaction(session ->
+                session.delete(id));
+    }
 }
