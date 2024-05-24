5f5dc2f48ce4254c0c5f7447fdd6316e4fe90cf8
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-24 15:40:28 +0300

    feat(001): Добавил атрибуты

--- src/main/java/ru/dragomirov/entities/Locations.java
+++ src/main/java/ru/dragomirov/entities/Locations.java
@@ -1,4 +1,28 @@
 package ru.dragomirov.entities;
 
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "Locations")
 public class Locations {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "user_id")
+    private int userId;
+    @Column(name = "latitude")
+    private BigDecimal latitude;
+    @Column(name = "longitude")
+    private BigDecimal longitude;
+
+    public Locations() {}
+
+    public Locations(String name, BigDecimal latitude, BigDecimal longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 }
