9c376d3bfedd43df63b2bb1e884c15a8d9113f0c
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-24 17:10:14 +0300

    chore(001): Реализовал логику для class HibernateSessionFactoryUtilTest

--- src/test/java/HibernateSessionFactoryUtilTest.java
+++ src/test/java/HibernateSessionFactoryUtilTest.java
@@ -1,2 +1,13 @@
+import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.Test;
+import ru.dragomirov.utils.HibernateSessionFactoryUtil;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class HibernateSessionFactoryUtilTest {
+    @Test
+    public void shouldGetSessionFactory() {
+        SessionFactory sessionFactory = HibernateSessionFactoryUtil.getSessionFactory();
+        assertNotNull(sessionFactory);
+    }
 }
