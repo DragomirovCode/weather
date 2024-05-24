9d66fc308b2ed6c2e9eb0b6c9255df596c9b8668
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-24 21:11:31 +0300

    feat(002): Реализовал class HibernateUserCrudDAO

--- src/main/java/ru/dragomirov/dao/HibernateUserCrudDAO.java
+++ src/main/java/ru/dragomirov/dao/HibernateUserCrudDAO.java
@@ -1,4 +1,41 @@
 package ru.dragomirov.dao;
 
-public class HibernateUserCrudDAO {
+import ru.dragomirov.entities.User;
+import ru.dragomirov.utils.HibernateSessionManagerUtil;
+
+import java.util.List;
+import java.util.Optional;
+
+public class HibernateUserCrudDAO implements CrudDAO<User, Integer> {
+    @Override
+    public void create(User entity) {
+        HibernateSessionManagerUtil.performTransaction(session ->
+                session.save(entity));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return HibernateSessionManagerUtil.performSessionQuery(session ->
+                session.createQuery("FROM User", User.class).list(),
+                "Произошла ошибка при выполнении метода 'findAll'(HibernateUserCrudDAO)");
+    }
+
+    @Override
+    public Optional<User> findById(Integer id) {
+        return Optional.ofNullable(HibernateSessionManagerUtil.performSessionQuery(session ->
+                session.get(User.class, id),
+                "Произошла ошибка при выполнении метода 'findById'(HibernateUserCrudDAO)"));
+    }
+
+    @Override
+    public void update(User entity) {
+        HibernateSessionManagerUtil.performTransaction(session ->
+                session.update(entity));
+    }
+
+    @Override
+    public void delete(Integer id) {
+        HibernateSessionManagerUtil.performTransaction(session ->
+                session.delete(id));
+    }
 }
