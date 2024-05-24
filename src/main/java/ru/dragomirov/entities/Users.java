337bc132762e92f6297705f404f235e839c55b3b
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-24 15:34:00 +0300

    feat(001): Добавил атрибуты

--- src/main/java/ru/dragomirov/entities/Users.java
+++ src/main/java/ru/dragomirov/entities/Users.java
@@ -1,4 +1,22 @@
 package ru.dragomirov.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Users")
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "login")
+    private String login;
+    @Column(name = "password")
+    private String password;
+
+    public Users() {}
+
+    public Users(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
 }
