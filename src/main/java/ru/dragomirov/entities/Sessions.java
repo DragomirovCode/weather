a51290604467e534a1f0c2d6dbe704a9f1766935
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-24 15:38:59 +0300

    feat(001): Добавил атрибуты

--- src/main/java/ru/dragomirov/entities/Sessions.java
+++ src/main/java/ru/dragomirov/entities/Sessions.java
@@ -1,4 +1,18 @@
 package ru.dragomirov.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Sessions")
 public class Sessions {
+    @Id
+    private String id;
+    @Column(name = "user_id")
+    private int userId;
+    @Column(name = "expires_at")
+    private LocalDateTime expiresAt;
 }
