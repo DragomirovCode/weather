ad7229d3f5dc43474d00612280979576a373ff72
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-24 16:04:17 +0300

    feat(001): Добавил маппинг для классов: Users, Session, Locations

--- src/main/java/ru/dragomirov/utils/HibernateSessionFactoryUtil.java
+++ src/main/java/ru/dragomirov/utils/HibernateSessionFactoryUtil.java
@@ -2,8 +2,11 @@ package ru.dragomirov.utils;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import ru.dragomirov.entities.Locations;
+import ru.dragomirov.entities.Users;
 
 public class HibernateSessionFactoryUtil {
     private final static HikariDataSource HIKARI_DATA_SOURCE;
@@ -22,6 +25,9 @@ public class HibernateSessionFactoryUtil {
         configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.SQLiteDialect");
         configuration.setProperty("hibernate.show_sql", "true");
         configuration.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        configuration.addAnnotatedClass(Users.class);
+        configuration.addAnnotatedClass(Session.class);
+        configuration.addAnnotatedClass(Locations.class);
 
         // Использование источника данных HikariCP вместо стандартного подключения Hibernate
         configuration.getProperties().put("hibernate.connection.datasource", HIKARI_DATA_SOURCE);
