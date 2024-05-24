63c7a270ffbe6a62592b42026bf9e8cfa11811a9
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-24 20:57:19 +0300

    feat(002): import lombok

--- src/main/java/ru/dragomirov/entities/Session.java
+++ src/main/java/ru/dragomirov/entities/Session.java
@@ -1,11 +1,16 @@
 package ru.dragomirov.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "Session")
 public class Session {
--- src/main/java/ru/dragomirov/entities/User.java
+++ src/main/java/ru/dragomirov/entities/User.java
@@ -1,7 +1,12 @@
 package ru.dragomirov.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "User")
 public class User {
