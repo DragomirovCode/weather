9bee818593e0233b4d45f1bdfc99594fbc6c32cf
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-24 15:23:53 +0300

    feat(000): Реализовал логику class HibernateSessionFactoryUtil

--- src/main/java/ru/dragomirov/utils/HibernateSessionFactoryUtil.java
+++ src/main/java/ru/dragomirov/utils/HibernateSessionFactoryUtil.java
@@ -1,4 +1,34 @@
 package ru.dragomirov.utils;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateSessionFactoryUtil {
+    private final static HikariDataSource HIKARI_DATA_SOURCE;
+    private static final SessionFactory sessionFactory;
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:sqlite::resource:db/database.db");
+        config.setDriverClassName("org.sqlite.JDBC");
+        config.setUsername("");
+        config.setPassword("");
+        HIKARI_DATA_SOURCE = new HikariDataSource(config);
+
+        // Создание конфигурации Hibernate
+        Configuration configuration = new Configuration();
+        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.SQLiteDialect");
+        configuration.setProperty("hibernate.show_sql", "true");
+        configuration.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+
+        // Использование источника данных HikariCP вместо стандартного подключения Hibernate
+        configuration.getProperties().put("hibernate.connection.datasource", HIKARI_DATA_SOURCE);
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 }
