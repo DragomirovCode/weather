bd999036aaa0b03ee2793913dc340ab7b4dac025
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-25 23:32:28 +0300

    feat(003): Добавил метод "doPost"

--- src/main/java/ru/dragomirov/servlets/RegistrationServlet.java
+++ src/main/java/ru/dragomirov/servlets/RegistrationServlet.java
@@ -6,8 +6,10 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import ru.dragomirov.dao.HibernateUserCrudDAO;
+import ru.dragomirov.entities.User;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @WebServlet(name = "RegistrationServlet", urlPatterns = "/registration")
 public class RegistrationServlet extends HttpServlet {
@@ -24,6 +26,34 @@ public class RegistrationServlet extends HttpServlet {
         } catch (Exception e) {
             req.setAttribute("errorMessage", "Ошибка при загрузке страницы.");
             req.getRequestDispatcher("/errors/serverError.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp){
+        try {
+            String login = req.getParameter("login");
+            String password = req.getParameter("password");
+
+            if (login.isEmpty() || password.isEmpty()) {
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                resp.getWriter().write("Ошибка: логин и пароль должны быть указаны");
+                return;
+            }
+
+            Optional<User> user = hibernateUserCrudDAO.findByLogin(login);
+
+            if (user.isPresent()) {
+                resp.setStatus(HttpServletResponse.SC_CONFLICT);
+                resp.getWriter().write("Ошибка: пользователь с таким логином уже существует");
+            }
+
+            User newUser = new User(login, password);
+            hibernateUserCrudDAO.create(newUser);
+
+        } catch (Exception e){
+            System.err.println("Произошла ошибка: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
