d1a26c2efb3cca125e5248163ba8732befec8163
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-25 23:15:07 +0300

    feat(003): добавил метод findByLogin

--- src/main/java/ru/dragomirov/dao/UserDAO.java
+++ src/main/java/ru/dragomirov/dao/UserDAO.java
@@ -2,5 +2,8 @@ package ru.dragomirov.dao;
 
 import ru.dragomirov.entities.User;
 
+import java.util.Optional;
+
 public interface UserDAO extends CrudDAO<User, Integer> {
+    Optional<User> findByLogin();
 }
