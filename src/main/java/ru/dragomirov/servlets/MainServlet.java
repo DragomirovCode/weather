8e4d877b97f5b0f3fb5da26abfc515f158942dc2
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-26 21:43:03 +0300

    fix(005): Пофиксил рекурсивную ошибку

--- src/main/java/ru/dragomirov/servlets/MainServlet.java
+++ src/main/java/ru/dragomirov/servlets/MainServlet.java
@@ -5,7 +5,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "MainServlet", urlPatterns = "/")
+@WebServlet(name = "MainServlet", urlPatterns = "")
 public class MainServlet extends HttpServlet {
     @Override
     public void init(){}
