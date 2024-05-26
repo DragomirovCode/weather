857b52da6b239833b7c50449769f584b0e1a7bc6
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-27 00:28:34 +0300

    feat(005): Добавил фильтр, который ограничевает доступ, если пользователь не вошёл в свой акк

--- src/main/java/ru/dragomirov/config/LoginFilter.java
+++ src/main/java/ru/dragomirov/config/LoginFilter.java
@@ -1,4 +1,28 @@
 package ru.dragomirov.config;
 
-public class LoginFilter {
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebFilter(value = {
+        "/"
+})
+public class LoginFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpSession session = request.getSession(false);
+
+        if (session == null || session.getAttribute("user") == null) {
+            response.sendRedirect("/login");
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
 }
