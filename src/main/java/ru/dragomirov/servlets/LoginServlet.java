26b74482ebd17a746435fd0cc55802670f743368
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-26 19:47:16 +0300

    feat(004): Реализовал метод "doGet"

--- src/main/java/ru/dragomirov/servlets/LoginServlet.java
+++ src/main/java/ru/dragomirov/servlets/LoginServlet.java
@@ -1,4 +1,25 @@
 package ru.dragomirov.servlets;
 
-public class LoginServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    public void init(){}
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            req.getRequestDispatcher("/login.html").forward(req, resp);
+        } catch (Exception e) {
+            req.setAttribute("errorMessage", "Ошибка при загрузке страницы.");
+            req.getRequestDispatcher("/errors/serverError.jsp").forward(req, resp);
+        }
+    }
 }
