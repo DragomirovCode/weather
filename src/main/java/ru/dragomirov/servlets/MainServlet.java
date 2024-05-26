6d421fcf03d19b7775e88a85ba721dcb27f7de4d
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-26 21:36:31 +0300

    .

--- src/main/java/ru/dragomirov/servlets/MainServlet.java
+++ src/main/java/ru/dragomirov/servlets/MainServlet.java
@@ -1,12 +1,9 @@
 package ru.dragomirov.servlets;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
 
 @WebServlet(name = "MainServlet", urlPatterns = "/")
 public class MainServlet extends HttpServlet {
@@ -14,12 +11,11 @@ public class MainServlet extends HttpServlet {
     public void init(){}
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         try {
-            req.getRequestDispatcher("/.html").forward(req, resp);
+            req.getRequestDispatcher("/main.html").forward(req, resp);
         } catch (Exception e) {
-            req.setAttribute("errorMessage", "Ошибка при загрузке страницы.");
-            req.getRequestDispatcher("/errors/serverError.jsp").forward(req, resp);
+            e.getMessage();
         }
     }
 }
