3341f88f342de9acd80281693918634674761e86
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-30 16:37:55 +0300

    feat(007): заполнил класс

--- src/main/java/ru/dragomirov/dto/response/LocationResponseDTO.java
+++ src/main/java/ru/dragomirov/dto/response/LocationResponseDTO.java
@@ -1,4 +1,10 @@
 package ru.dragomirov.dto.response;
 
+import java.math.BigDecimal;
+
 public class LocationResponseDTO {
+    public int id;
+    public String name;
+    public BigDecimal latitude;
+    public BigDecimal longitude;
 }
