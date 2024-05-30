ced7fc9132b14f02b04e03863e2a45133d4fea56
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-30 16:50:31 +0300

    feat(008): добавил метод "init" и метод "doGet"

--- src/main/java/ru/dragomirov/servlets/RequestJsonServlet.java
+++ src/main/java/ru/dragomirov/servlets/RequestJsonServlet.java
@@ -2,7 +2,14 @@ package ru.dragomirov.servlets;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "RequestJsonServlet", urlPatterns = "/search-by-name")
 public class RequestJsonServlet extends HttpServlet {
+    @Override
+    public void init(){}
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) { }
 }
