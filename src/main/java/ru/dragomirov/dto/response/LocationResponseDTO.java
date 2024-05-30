d133fe47390034f55f5bfb88e7f1d03fd3fbe790
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-30 16:38:29 +0300

    feat(007): добавил lombok

--- src/main/java/ru/dragomirov/dto/response/LocationResponseDTO.java
+++ src/main/java/ru/dragomirov/dto/response/LocationResponseDTO.java
@@ -1,7 +1,12 @@
 package ru.dragomirov.dto.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Getter
+@Setter
 public class LocationResponseDTO {
     public int id;
     public String name;
