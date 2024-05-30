a3190c7620c28a3aed695e852c6c532ad7a24ae7
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-30 18:36:36 +0300

    feat(008): реализовал метод "locationToEntity"

--- src/main/java/ru/dragomirov/utils/MappingUtil.java
+++ src/main/java/ru/dragomirov/utils/MappingUtil.java
@@ -1,4 +1,17 @@
 package ru.dragomirov.utils;
 
+import org.modelmapper.ModelMapper;
+import ru.dragomirov.dto.request.LocationRequestDTO;
+import ru.dragomirov.entities.Location;
+
 public class MappingUtil {
+    private static final ModelMapper MODEL_MAPPER;
+
+    static {
+        MODEL_MAPPER = new ModelMapper();
+    }
+
+    public static Location locationToEntity(LocationRequestDTO dto) {
+        return MODEL_MAPPER.map(dto, Location.class);
+    }
 }
