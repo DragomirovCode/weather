979e027a3b13a56debbfe651760f037475a5f1bb
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-06-01 01:21:45 +0300

    feat(012): добавил LOGIN_PAGE_X, REGISTRATION_PAGE_X

--- src/main/java/ru/dragomirov/utils/constants/WebPageConstants.java
+++ src/main/java/ru/dragomirov/utils/constants/WebPageConstants.java
@@ -2,7 +2,9 @@ package ru.dragomirov.utils.constants;
 
 public enum WebPageConstants {
     LOGIN_PAGE("/login.html"),
-    REGISTRATION_PAGE("/registration.html");
+    LOGIN_PAGE_X("/login"),
+    REGISTRATION_PAGE("/registration.html"),
+    REGISTRATION_PAGE_X("/registration");
     private final String value;
 
     WebPageConstants(String value) {
