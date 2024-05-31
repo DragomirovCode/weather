77141a91fc46f3854722c480b3c14eb549067eb5
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-31 03:40:04 +0300

    feat(010): переопределил метод "service"

--- src/main/java/ru/dragomirov/servlets/BaseServlet.java
+++ src/main/java/ru/dragomirov/servlets/BaseServlet.java
@@ -1,4 +1,19 @@
 package ru.dragomirov.servlets;
 
-public class BaseServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class BaseServlet extends HttpServlet {
+    @Override
+    public void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            super.service(req, resp);
+        } catch (IOException | ServletException e) {
+            HttpErrorHandlingServlet.handleError(500, resp, e.getMessage());
+        }
+    }
 }
