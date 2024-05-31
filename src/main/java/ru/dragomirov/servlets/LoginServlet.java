55824179a5618cc23542cab99775dfb37fe6da24
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-31 03:46:31 +0300

    feat(010): extends BaseServlet

--- src/main/java/ru/dragomirov/servlets/LoginServlet.java
+++ src/main/java/ru/dragomirov/servlets/LoginServlet.java
@@ -13,69 +13,58 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
-public class LoginServlet extends HttpServlet {
+public class LoginServlet extends BaseServlet {
     private HibernateUserCrudDAO hibernateUserCrudDAO;
     private HibernateSessionCrudDAO hibernateSessionCrudDAO;
+
     @Override
-    public void init(){
+    public void init() {
         this.hibernateUserCrudDAO = new HibernateUserCrudDAO();
         this.hibernateSessionCrudDAO = new HibernateSessionCrudDAO();
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try {
-            req.getRequestDispatcher("/login.html").forward(req, resp);
-        } catch (Exception e) {
-            req.setAttribute("errorMessage", "Ошибка при загрузке страницы.");
-            req.getRequestDispatcher("/errors/serverError.jsp").forward(req, resp);
-        }
+        req.getRequestDispatcher("/login.html").forward(req, resp);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
-        try {
-            String login = req.getParameter("login");
-            String password = req.getParameter("password");
-            String button = req.getParameter("button");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        String button = req.getParameter("button");
 
+        if (login.isEmpty() || password.isEmpty()) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().write("Ошибка: логин и пароль должны быть указаны");
+            return;
+        }
+        switch (button) {
+            case "login":
+                Optional<User> user = hibernateUserCrudDAO.findByLoginAndPassword(login, password);
+                if (user.isPresent()) {
+                    LocalDateTime nowTime = LocalDateTime.now();
+                    LocalDateTime futureTime = nowTime.plusHours(1);
+                    Optional<Session> uuid = hibernateSessionCrudDAO.findByUserId(user.get().getId());
+                    HttpSession session = req.getSession();
+                    session.setAttribute("user", uuid.get().getUserId());
 
-            if (login.isEmpty() || password.isEmpty()) {
-                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                resp.getWriter().write("Ошибка: логин и пароль должны быть указаны");
-                return;
-            }
+                    Session sessionUpdateTime = new Session(uuid.get().getId(), user.get().getId(), futureTime);
+                    hibernateSessionCrudDAO.update(sessionUpdateTime);
 
-            switch (button) {
-                case "login":
-                    Optional<User> user = hibernateUserCrudDAO.findByLoginAndPassword(login, password);
-                    if (user.isPresent()) {
-                        LocalDateTime nowTime = LocalDateTime.now();
-                        LocalDateTime futureTime = nowTime.plusHours(1);
-                        Optional<Session> uuid = hibernateSessionCrudDAO.findByUserId(user.get().getId());
-                        HttpSession session = req.getSession();
-                        session.setAttribute("user",uuid.get().getUserId());
+                    Cookie cookie = new Cookie("uuid", uuid.get().getId());
+                    cookie.setMaxAge(40);
+                    resp.addCookie(cookie);
 
-                        Session sessionUpdateTime = new Session(uuid.get().getId(), user.get().getId(), futureTime);
-                        hibernateSessionCrudDAO.update(sessionUpdateTime);
-
-                        Cookie cookie = new Cookie("uuid", uuid.get().getId());
-                        cookie.setMaxAge(40);
-                        resp.addCookie(cookie);
-
-                        resp.sendRedirect("/?uuid=" + uuid.get().getId());
-                    } else {
-                        resp.setStatus(HttpServletResponse.SC_CONFLICT);
-                        resp.getWriter().write("Ошибка: пользователя с таким логином или паролем не существует");
-                    }
-                    break;
-                case "registration":
-                    resp.sendRedirect("/registration");
-                    break;
-            }
-        } catch (Exception e) {
-            System.err.println("Произошла ошибка: " + e.getMessage());
-            e.printStackTrace();
+                    resp.sendRedirect("/?uuid=" + uuid.get().getId());
+                } else {
+                    resp.setStatus(HttpServletResponse.SC_CONFLICT);
+                    resp.getWriter().write("Ошибка: пользователя с таким логином или паролем не существует");
+                }
+                break;
+            case "registration":
+                resp.sendRedirect("/registration");
+                break;
         }
     }
 }
