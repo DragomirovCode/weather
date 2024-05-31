634a7291998d1cd1804719f95019c0896728395d
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-31 03:35:39 +0300

    feat(010): реализовал метод "httpErrors"

--- src/main/java/ru/dragomirov/errorhandling/Http500ErrorHandler.java
+++ src/main/java/ru/dragomirov/errorhandling/Http500ErrorHandler.java
@@ -1,4 +1,14 @@
 package ru.dragomirov.errorhandling;
 
-public class Http500ErrorHandler {
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class Http500ErrorHandler implements ErrorHandler{
+    @Override
+    public void httpErrors(HttpServletResponse resp, String errorMessage) throws IOException {
+        resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        String errorResponse = "{\"message\": \"" + errorMessage + "\"}";
+        resp.getWriter().write(errorResponse);
+    }
 }
