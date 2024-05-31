51bfd84a4667361cd593cc052ec2f66f9995f10f
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-31 22:36:40 +0300

    feat(011): добавил атрибуты и конструктор

--- src/main/java/ru/dragomirov/requests/LoginRequest.java
+++ src/main/java/ru/dragomirov/requests/LoginRequest.java
@@ -1,4 +1,15 @@
 package ru.dragomirov.requests;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 public class LoginRequest {
+    private String login;
+    private String password;
+    private String button;
+
+    public LoginRequest(HttpServletRequest req) {
+        this.login = req.getParameter("login");
+        this.password = req.getParameter("password");
+        this.button = req.getParameter("button");
+    }
 }
