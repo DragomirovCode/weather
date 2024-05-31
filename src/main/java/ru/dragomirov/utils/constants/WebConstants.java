421311f034b17adf82d95a3c66080a6d334aed72
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-31 23:50:55 +0300

    feat(012): добавил константы: "LOGIN_PAGE", "USER_ATTRIBUTE", "UUID_COOKIE_NAME"

--- src/main/java/ru/dragomirov/utils/constants/WebConstants.java
+++ src/main/java/ru/dragomirov/utils/constants/WebConstants.java
@@ -1,4 +1,16 @@
 package ru.dragomirov.utils.constants;
 
 public enum WebConstants {
+    LOGIN_PAGE("/login.html"),
+    USER_ATTRIBUTE("user"),
+    UUID_COOKIE_NAME("uuid");
+    private final String value;
+
+    WebConstants(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
