fedcd7cdb538ae0cfde57b16245d21cbc591b379
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-31 23:41:36 +0300

    feat(012): создал enum ApiKeyConstant

---
+++ src/main/java/ru/dragomirov/utils/constants/ApiKeyConstant.java
@@ -0,0 +1,14 @@
+package ru.dragomirov.utils.constants;
+
+public enum ApiKeyConstant {
+    API_KEY_CONSTANT("66b4f9f34b5073497f497f4d4f2be438");
+    private final String value;
+
+    ApiKeyConstant(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
