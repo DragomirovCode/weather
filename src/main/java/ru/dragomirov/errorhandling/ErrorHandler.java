4d273faff50c6441045d80bd503e68d61dbbb904
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-31 03:11:52 +0300

    feat(010): добавил метод "httpErrors"

--- src/main/java/ru/dragomirov/errorhandling/ErrorHandler.java
+++ src/main/java/ru/dragomirov/errorhandling/ErrorHandler.java
@@ -1,4 +1,9 @@
 package ru.dragomirov.errorhandling;
 
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
 public interface ErrorHandler {
+    void httpErrors(HttpServletResponse resp, String errorMessage) throws IOException;
 }
