163f56a0b3dc7f4876281b50360bcaf55d7b8ef9
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-06-30 19:23:08 +0300

    chore(008): Убрал лишний код

--- src/main/java/ru/dragomirov/servlets/UniqueCityWeatherServlet.java
+++ src/main/java/ru/dragomirov/servlets/UniqueCityWeatherServlet.java
@@ -3,7 +3,6 @@ package ru.dragomirov.servlets;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.http.HttpResponse;
@@ -19,6 +18,7 @@ import ru.dragomirov.utils.MappingUtil;
 import ru.dragomirov.utils.Utils;
 import ru.dragomirov.utils.constants.ApiKeyConstant;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,6 +26,7 @@ import java.util.stream.Collectors;
 public class UniqueCityWeatherServlet extends BaseServlet {
     private HibernateLocationCrudDAO hibernateLocationCrudDAO;
     private Utils utils;
+
     @Override
     public void init(){
         this.hibernateLocationCrudDAO = new HibernateLocationCrudDAO();
@@ -33,35 +34,31 @@ public class UniqueCityWeatherServlet extends BaseServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        try {
-            String cityName = req.getParameter("city");
-            String apiKey = ApiKeyConstant.API_KEY_CONSTANT.getValue();
-            String apiUrl = utils.buildUniqueCityWeatherApiUrl(cityName, apiKey);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String cityName = req.getParameter("city");
+        String apiKey = ApiKeyConstant.API_KEY_CONSTANT.getValue();
+        String apiUrl = utils.buildUniqueCityWeatherApiUrl(cityName, apiKey);
 
-            CloseableHttpClient httpClient = HttpClients.createDefault();
-            // Создание объекта запроса
-            HttpGet request = new HttpGet(apiUrl);
-            // Выполнение запроса
-            HttpResponse response = httpClient.execute(request);
-            // Преобразование тела ответа в строку
-            String jsonStr = EntityUtils.toString(response.getEntity());
-            // Десериализация строки JSON в Java-объект
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            LocationRequestDTO requestDTO = gson.fromJson(jsonStr, LocationRequestDTO.class);
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        // Создание объекта запроса
+        HttpGet request = new HttpGet(apiUrl);
+        // Выполнение запроса
+        HttpResponse response = httpClient.execute(request);
+        // Преобразование тела ответа в строку
+        String jsonStr = EntityUtils.toString(response.getEntity());
+        // Десериализация строки JSON в Java-объект
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        LocationRequestDTO requestDTO = gson.fromJson(jsonStr, LocationRequestDTO.class);
 
-            Location location = MappingUtil.locationToEntity(requestDTO);
-            hibernateLocationCrudDAO.create(location);
+        Location location = MappingUtil.locationToEntity(requestDTO);
+        hibernateLocationCrudDAO.create(location);
 
-            List<Location> locationList = hibernateLocationCrudDAO.findAll();
-            List<LocationResponseDTO> responseDTOList = locationList.stream()
+        List<Location> locationList = hibernateLocationCrudDAO.findAll();
+        List<LocationResponseDTO> responseDTOList = locationList.stream()
                     .map(MappingUtil::locationToDTO).collect(Collectors.toList());
 
-            String jsonResponse = gson.toJson(responseDTOList);
-            resp.getWriter().write(jsonResponse);
-            resp.setStatus(HttpServletResponse.SC_OK);
-        } catch (Exception e) {
-            e.getMessage();
-        }
+        String jsonResponse = gson.toJson(responseDTOList);
+        resp.getWriter().write(jsonResponse);
+        resp.setStatus(HttpServletResponse.SC_OK);
     }
 }
