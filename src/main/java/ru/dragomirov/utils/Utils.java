21639ad0a299b64f519950ac1e19547d8d79090f
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-06-30 19:10:33 +0300

    feat(008): Создал метод "buildUniqueCityWeatherApiUrl"

--- src/main/java/ru/dragomirov/utils/Utils.java
+++ src/main/java/ru/dragomirov/utils/Utils.java
@@ -1,4 +1,7 @@
 package ru.dragomirov.utils;
 
 public class Utils {
+    public String buildUniqueCityWeatherApiUrl(String city, String key) {
+        return "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key;
+    }
 }
