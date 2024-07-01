503d89d0c4defc514a0e28d84af66de339927b45
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-01 17:30:44 +0300

    feat(008-0.1): Создал class JsonResponseFilter

---
+++ src/main/java/ru/dragomirov/config/JsonResponseFilter.java
@@ -0,0 +1,32 @@
+package ru.dragomirov.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(value = {
+        "/unique-search-city-weather/*"
+})
+public class JsonResponseFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+        servletRequest.setCharacterEncoding("UTF-8");
+        httpResponse.setCharacterEncoding("UTF-8");
+        httpResponse.setContentType("application/json");
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
