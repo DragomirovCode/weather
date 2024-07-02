3c0cb6768103b7029f6683157704c294aff4eb3c
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-02 16:35:42 +0300

    feat(008-0.3.1): Добавил метод init

--- src/main/java/ru/dragomirov/servlets/MainFormServlet.java
+++ src/main/java/ru/dragomirov/servlets/MainFormServlet.java
@@ -3,4 +3,7 @@ package ru.dragomirov.servlets;
 import jakarta.servlet.annotation.WebServlet;
 
 @WebServlet(name = "MainFormServlet", urlPatterns = "/my")
-public class MainFormServlet extends BaseServlet {}
+public class MainFormServlet extends BaseServlet {
+    @Override
+    public void init() {}
+}
