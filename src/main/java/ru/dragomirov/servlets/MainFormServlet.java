26d55684a5be8d9af24e1a5e145830a0d171c8c3
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-02 16:37:17 +0300

    feat(008-0.3.1): Добавил метод doGet

--- src/main/java/ru/dragomirov/servlets/MainFormServlet.java
+++ src/main/java/ru/dragomirov/servlets/MainFormServlet.java
@@ -1,9 +1,14 @@
 package ru.dragomirov.servlets;
 
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "MainFormServlet", urlPatterns = "/my")
 public class MainFormServlet extends BaseServlet {
     @Override
     public void init() {}
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp){}
 }
