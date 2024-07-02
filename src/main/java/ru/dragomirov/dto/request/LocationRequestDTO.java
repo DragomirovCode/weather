0ec1bc69e721b0c265b6c2cd9c418ee52a4e85e0
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-02 16:11:04 +0300

    feat(008-0.3): Добавил class Main

--- src/main/java/ru/dragomirov/dto/request/LocationRequestDTO.java
+++ src/main/java/ru/dragomirov/dto/request/LocationRequestDTO.java
@@ -13,6 +13,8 @@ public class LocationRequestDTO {
     public String name;
     @SerializedName("coord")
     public Coordinates coordinates;
+    @SerializedName("main")
+    public Main main;
 
     @Getter
     @Setter
@@ -21,5 +23,18 @@ public class LocationRequestDTO {
         public BigDecimal latitude;
         @SerializedName("lon")
         public BigDecimal longitude;
+    }
+
+    @Getter
+    @Setter
+    public static class Main {
+        @SerializedName("temp")
+        public BigDecimal temperatureActual;
+        @SerializedName("temp_max")
+        public BigDecimal temperatureMax;
+        @SerializedName("temp_min")
+        public BigDecimal temperatureMin;
+        @SerializedName("feels_like")
+        public BigDecimal temperatureFeelsLike;
     }
 }
