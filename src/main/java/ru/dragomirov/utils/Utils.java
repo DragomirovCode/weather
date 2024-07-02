32d0494954e801fd50982a615320dc5b939cc703
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-02 12:55:10 +0300

    feat(008-0.1): Добавил логику замены пробелов на "+", чтобы он корректно кодировал имя города

--- src/main/java/ru/dragomirov/utils/Utils.java
+++ src/main/java/ru/dragomirov/utils/Utils.java
@@ -1,7 +1,11 @@
 package ru.dragomirov.utils;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 public class Utils {
     public String buildUniqueCityWeatherApiUrl(String city, String key) {
-        return "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key;
+        String encodedCity = URLEncoder.encode(city, StandardCharsets.UTF_8);
+        return "https://api.openweathermap.org/data/2.5/weather?q=" + encodedCity + "&appid=" + key;
     }
 }
