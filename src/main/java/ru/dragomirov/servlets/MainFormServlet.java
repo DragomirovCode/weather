8c0d813cd8b27f02a8b1c21b79f5edfe89430c02
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-03 12:44:59 +0300

    feat(008-0.3.1): Добавил логику в метод doGet

--- src/main/java/ru/dragomirov/servlets/MainFormServlet.java
+++ src/main/java/ru/dragomirov/servlets/MainFormServlet.java
@@ -1,14 +1,56 @@
 package ru.dragomirov.servlets;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import ru.dragomirov.dao.HibernateLocationCrudDAO;
+import ru.dragomirov.dao.HibernateSessionCrudDAO;
+import ru.dragomirov.dto.response.LocationResponseDTO;
+import ru.dragomirov.entities.Location;
+import ru.dragomirov.entities.Session;
+import ru.dragomirov.utils.MappingUtil;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @WebServlet(name = "MainFormServlet", urlPatterns = "/my")
-public class MainFormServlet extends BaseServlet {
-    @Override
-    public void init() {}
+public class MainFormServlet extends HttpServlet {
+    private HibernateLocationCrudDAO hibernateLocationCrudDAO;
+    private HibernateSessionCrudDAO hibernateSessionCrudDAO;
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp){}
+    public void init() {
+        this.hibernateLocationCrudDAO = new HibernateLocationCrudDAO();
+        this.hibernateSessionCrudDAO = new HibernateSessionCrudDAO();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String otherUuid = (String) getServletContext().getAttribute("myUuid");
+
+        if (otherUuid == null || otherUuid.isEmpty()) {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Пользователя с таким uuid не существует");
+            return;
+        }
+
+        Optional<Session> session = hibernateSessionCrudDAO.findById(otherUuid);
+
+        System.out.println(session.get().getUserId());
+
+        List<Location> locationList = hibernateLocationCrudDAO.findByListLocationId(session.get().getUserId());
+
+        List<LocationResponseDTO> responseDTOList = locationList.stream()
+                .map(MappingUtil::locationToDTO)
+                .collect(Collectors.toList());
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(responseDTOList);
+        resp.getWriter().write(json);
+        req.setAttribute("jsonData", json);
+    }
 }
