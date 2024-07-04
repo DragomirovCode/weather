25f60ec333f2101260d9f8c43ca60529f8ddd389
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-04 11:16:35 +0300

    feat(008-0.3.1.1): Добавил логику

--- src/main/java/ru/dragomirov/config/thymeleaf/TemplateListener.java
+++ src/main/java/ru/dragomirov/config/thymeleaf/TemplateListener.java
@@ -1,4 +1,17 @@
 package ru.dragomirov.config.thymeleaf;
 
-public class TemplateListener {
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import org.thymeleaf.TemplateEngine;
+
+@WebListener
+public class TemplateListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext servletContext = sce.getServletContext();
+        TemplateEngine templateEngine = TemplateEngineConfig.buildTemplateEngine(servletContext);
+        servletContext.setAttribute("templateEngine", templateEngine);
+    }
 }
