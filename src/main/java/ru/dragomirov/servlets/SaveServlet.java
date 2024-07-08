a1f0162b4aae36d0c371c8c684d76ef09dc97bf4
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-08 11:01:23 +0300

    feat(008-v3): Добавил логику для метода "doPost"

--- src/main/java/ru/dragomirov/servlets/SaveServlet.java
+++ src/main/java/ru/dragomirov/servlets/SaveServlet.java
@@ -1,4 +1,36 @@
 package ru.dragomirov.servlets;
 
-public class SaveServlet {
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ru.dragomirov.dao.HibernateLocationCrudDAO;
+import ru.dragomirov.entities.Location;
+
+import java.math.BigDecimal;
+
+@WebServlet(name = "SaveServlet", urlPatterns = "/save")
+public class SaveServlet extends BaseServlet {
+    private HibernateLocationCrudDAO hibernateLocationCrudDAO;
+
+    @Override
+    public void init() {
+        this.hibernateLocationCrudDAO = new HibernateLocationCrudDAO();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        String id = req.getParameter("id");
+        String city = req.getParameter("city");
+        String latitude = req.getParameter("latitude");
+        String longitude = req.getParameter("longitude");
+
+        Location location = new Location();
+        location.setId(Integer.parseInt(id));
+        location.setName(city);
+        location.setLatitude(BigDecimal.valueOf(Double.parseDouble(latitude)));
+        location.setLongitude(BigDecimal.valueOf(Double.parseDouble(longitude)));
+        location.setUserId(1);
+
+        hibernateLocationCrudDAO.create(location);
+    }
 }
