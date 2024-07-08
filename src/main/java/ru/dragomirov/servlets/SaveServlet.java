6adc7ff507e91651fd035f56c79efda13927b042
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-08 11:43:11 +0300

    feat(008-v3): Обновил метод "doPost"

--- src/main/java/ru/dragomirov/servlets/SaveServlet.java
+++ src/main/java/ru/dragomirov/servlets/SaveServlet.java
@@ -4,21 +4,27 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import ru.dragomirov.dao.HibernateLocationCrudDAO;
+import ru.dragomirov.dao.HibernateSessionCrudDAO;
 import ru.dragomirov.entities.Location;
+import ru.dragomirov.entities.Session;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Optional;
 
 @WebServlet(name = "SaveServlet", urlPatterns = "/save")
 public class SaveServlet extends BaseServlet {
     private HibernateLocationCrudDAO hibernateLocationCrudDAO;
+    private HibernateSessionCrudDAO hibernateSessionCrudDAO;
 
     @Override
     public void init() {
         this.hibernateLocationCrudDAO = new HibernateLocationCrudDAO();
+        this.hibernateSessionCrudDAO = new HibernateSessionCrudDAO();
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String id = req.getParameter("id");
         String city = req.getParameter("city");
         String latitude = req.getParameter("latitude");
@@ -29,8 +35,20 @@ public class SaveServlet extends BaseServlet {
         location.setName(city);
         location.setLatitude(BigDecimal.valueOf(Double.parseDouble(latitude)));
         location.setLongitude(BigDecimal.valueOf(Double.parseDouble(longitude)));
-        location.setUserId(1);
+
+        String otherUuid = (String) getServletContext().getAttribute("myUuid");
+
+        if (otherUuid == null || otherUuid.isEmpty()) {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Пользователя с таким uuid не существует");
+            return;
+        }
+
+        Optional<Session> session = hibernateSessionCrudDAO.findById(otherUuid);
+
+        location.setUserId(session.get().getUserId());
 
         hibernateLocationCrudDAO.create(location);
+
+        resp.sendRedirect("/?uuid=" + session.get().getId());
     }
 }
