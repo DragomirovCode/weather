b9e9437069b80c13e7a801086978114a3e251e86
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-10 15:41:57 +0300

    feat(010): добавил метод "findByLocationName"

--- src/main/java/ru/dragomirov/dao/LocationDAO.java
+++ src/main/java/ru/dragomirov/dao/LocationDAO.java
@@ -3,8 +3,10 @@ package ru.dragomirov.dao;
 import ru.dragomirov.entities.Location;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LocationDAO extends CrudDAO<Location, Integer> {
     List<Location> findByListLocationName(String name);
     List<Location> findByListLocationUserId(int userId);
+    Optional<Location> findByLocationName(String name);
 }
