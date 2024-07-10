8998421923ec3c3cb26f0906e4bfa3854189a6ca
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-10 16:35:34 +0300

    feat(011): реализовал метод doGet

--- src/main/java/ru/dragomirov/servlets/BackServlet.java
+++ src/main/java/ru/dragomirov/servlets/BackServlet.java
@@ -1,4 +1,27 @@
 package ru.dragomirov.servlets;
 
-public class BackServlet {
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ru.dragomirov.dao.HibernateSessionCrudDAO;
+import ru.dragomirov.entities.Session;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet(name = "BackServlet", urlPatterns = "/back")
+public class BackServlet extends BaseServlet {
+    private HibernateSessionCrudDAO hibernateSessionCrudDAO;
+
+    @Override
+    public void init() {
+        this.hibernateSessionCrudDAO = new HibernateSessionCrudDAO();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String otherUuid = (String) getServletContext().getAttribute("myUuid");
+        Optional<Session> session = hibernateSessionCrudDAO.findById(otherUuid);
+        resp.sendRedirect("/?uuid=" + session.get().getId());
+    }
 }
