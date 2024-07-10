2695edb62312047d55d2df1365a1e6399ec1a22c
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-10 13:43:38 +0300

    chore(010): Удалил ненужный код

--- src/main/java/ru/dragomirov/utils/Utils.java
+++ src/main/java/ru/dragomirov/utils/Utils.java
@@ -5,7 +5,7 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 public class Utils {
-    public String buildUniqueCityWeatherApiUrl(String city, String key) {
+    public String buildCityWeatherApiUrl(String city, String key) {
         String encodedCity = URLEncoder.encode(city, StandardCharsets.UTF_8);
         return "https://api.openweathermap.org/data/2.5/weather?q=" + encodedCity + "&appid=" + key;
     }
