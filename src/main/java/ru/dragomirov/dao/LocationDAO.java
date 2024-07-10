5949052ad7cf5ff30272d17834a38438f6eaf67f
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-10 16:06:08 +0300

    feat(010): добавил метод "findByLocationLatitudeAndLongitude"

--- src/main/java/ru/dragomirov/dao/LocationDAO.java
+++ src/main/java/ru/dragomirov/dao/LocationDAO.java
@@ -2,6 +2,7 @@ package ru.dragomirov.dao;
 
 import ru.dragomirov.entities.Location;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,4 +10,5 @@ public interface LocationDAO extends CrudDAO<Location, Integer> {
     List<Location> findByListLocationName(String name);
     List<Location> findByListLocationUserId(int userId);
     Optional<Location> findByLocationName(String name);
+    Optional<Location> findByLocationLatitudeAndLongitude(BigDecimal latitude, BigDecimal longitude);
 }
