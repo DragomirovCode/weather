491ca4191866cdda9d30914de81e11fe8bce0b68
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-10 14:49:19 +0300

    feat(010): Добавил обработку patch запроса

--- src/main/java/ru/dragomirov/servlets/DeleteServlet.java
+++ src/main/java/ru/dragomirov/servlets/DeleteServlet.java
@@ -1,4 +1,25 @@
 package ru.dragomirov.servlets;
 
-public class DeleteServlet {
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "DeleteServlet", urlPatterns = "/delete")
+public class DeleteServlet extends BaseServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String method = req.getMethod();
+        String overrideMethod = req.getParameter("_method");
+        if ("POST".equalsIgnoreCase(method) && "PATCH".equalsIgnoreCase(overrideMethod)) {
+            doPatch(req, resp);
+        } else {
+            super.service(req, resp);
+        }
+    }
+
+    protected void doPatch(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().write("PATCH request processed");
+    }
 }
