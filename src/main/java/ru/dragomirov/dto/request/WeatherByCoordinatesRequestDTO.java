f857ac71810986a49917a903c41f7dbecc802f6a
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-11 16:16:40 +0300

    feat(012): добавил атрибуты

--- src/main/java/ru/dragomirov/dto/request/WeatherByCoordinatesRequestDTO.java
+++ src/main/java/ru/dragomirov/dto/request/WeatherByCoordinatesRequestDTO.java
@@ -1,4 +1,37 @@
 package ru.dragomirov.dto.request;
 
+import com.google.gson.annotations.SerializedName;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
 public class WeatherByCoordinatesRequestDTO {
+    public int id;
+    public String name;
+    @SerializedName("coord")
+    public Coordinates coordinates;
+    @SerializedName("main")
+    public Main main;
+
+    @Getter
+    @Setter
+    public static class Coordinates {
+        @SerializedName("lat")
+        public BigDecimal latitude;
+        @SerializedName("lon")
+        public BigDecimal longitude;
+    }
+
+    @Getter
+    @Setter
+    public static class Main {
+        @SerializedName("temp")
+        public BigDecimal temperatureActual;
+        @SerializedName("temp_max")
+        public BigDecimal temperatureMax;
+        @SerializedName("temp_min")
+        public BigDecimal temperatureMin;
+        @SerializedName("feels_like")
+        public BigDecimal temperatureFeelsLike;
+    }
 }
