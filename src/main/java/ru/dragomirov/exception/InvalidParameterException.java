9bf983ea393e7ac9c4eddf195a4ad7faaa76a1e3
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-12 12:34:21 +0300

    feat(013): добавил class InvalidParameterException

---
+++ src/main/java/ru/dragomirov/exception/InvalidParameterException.java
@@ -0,0 +1,7 @@
+package ru.dragomirov.exception;
+
+public class InvalidParameterException extends RuntimeException {
+    public InvalidParameterException(String message) {
+        super(message);
+    }
+}
