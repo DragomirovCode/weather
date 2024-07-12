b5afc1484b3cf93e8992f917726b82dfef205863
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-12 15:05:40 +0300

    feat(013): добавил class SessionExpiredException

---
+++ src/main/java/ru/dragomirov/exception/SessionExpiredException.java
@@ -0,0 +1,7 @@
+package ru.dragomirov.exception;
+
+public class SessionExpiredException extends RuntimeException {
+    public SessionExpiredException(String message) {
+        super(message);
+    }
+}
