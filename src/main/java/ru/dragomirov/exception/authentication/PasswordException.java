79f14a12e95784efdc212aeaa915e871ff6e5da8
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-14 23:11:18 +0300

    feat(017): добавил "class PasswordException"

---
+++ src/main/java/ru/dragomirov/exception/authentication/PasswordException.java
@@ -0,0 +1,7 @@
+package ru.dragomirov.exception.authentication;
+
+public class PasswordException extends RuntimeException{
+    public PasswordException(String message) {
+        super(message);
+    }
+}
