39f1a15d3dad5f09b4fc246c465d5375680a7fcd
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-16 11:52:57 +0300

    feat(018): добавил метод "shouldThrowLoginException"

--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -2,9 +2,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+import ru.dragomirov.dao.HibernateUserCrudDAO;
+import ru.dragomirov.entities.User;
+import ru.dragomirov.exception.authentication.LoginException;
 import ru.dragomirov.services.RegistrationService;
 import ru.dragomirov.utils.request.AuthenticationRequest;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 public class UserRegistrationTest {
@@ -34,5 +38,30 @@ public class UserRegistrationTest {
 
         // Проверяем, что был выполнен редирект на страницу логина
         verify(resp).sendRedirect("/login");
+    }
+
+    @Test
+    public void shouldThrowLoginException() {
+        // Создаем mock объекты
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest(req);
+        RegistrationService registrationService = new RegistrationService();
+        HibernateUserCrudDAO hibernateUserCrudDAO = new HibernateUserCrudDAO();
+        User user = new User();
+        user.setLogin("test");
+        user.setPassword("1");
+        hibernateUserCrudDAO.create(user);
+
+        // Устанавливаем параметры
+        authenticationRequest.setLogin("test");
+        authenticationRequest.setPassword("1");
+        authenticationRequest.setButton("registration");
+
+        // Проверяем, что выбрасывается исключение при вызове метода
+        assertThrows(LoginException.class, () -> {
+            registrationService.processAuthenticationRequest(authenticationRequest, resp);
+        });
     }
 }
