3c68e8261af46820cf5fc37c3eafdc68bdfb31c7
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-16 10:54:38 +0300

    feat(018): изменил HibernateSessionFactoryUtil

--- src/main/java/ru/dragomirov/utils/HibernateSessionFactoryUtil.java
+++ src/main/java/ru/dragomirov/utils/HibernateSessionFactoryUtil.java
@@ -1,36 +1,19 @@
 package ru.dragomirov.utils;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import ru.dragomirov.entities.Location;
-import ru.dragomirov.entities.Session;
-import ru.dragomirov.entities.User;
+import ru.dragomirov.exception.DatabaseOperationException;
 
 public class HibernateSessionFactoryUtil {
-    private final static HikariDataSource HIKARI_DATA_SOURCE;
     private static final SessionFactory sessionFactory;
 
     static {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:postgresql://localhost:5433/weather");
-        config.setDriverClassName("org.postgresql.Driver");
-        config.setUsername("postgres");
-        config.setPassword("Sdsafcxvre2341");
-        HIKARI_DATA_SOURCE = new HikariDataSource(config);
-
-        // Создание конфигурации Hibernate
-        Configuration configuration = new Configuration();
-        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        configuration.setProperty("hibernate.show_sql", "true");
-        configuration.addAnnotatedClass(User.class);
-        configuration.addAnnotatedClass(Session.class);
-        configuration.addAnnotatedClass(Location.class);
-
-        // Использование источника данных HikariCP вместо стандартного подключения Hibernate
-        configuration.getProperties().put("hibernate.connection.datasource", HIKARI_DATA_SOURCE);
-        sessionFactory = configuration.buildSessionFactory();
+        try {
+            Configuration configuration = new Configuration().configure();
+            sessionFactory = configuration.buildSessionFactory();
+        } catch (DatabaseOperationException e) {
+            throw new DatabaseOperationException("an error occurred in the database");
+        }
     }
 
     public static SessionFactory getSessionFactory() {
