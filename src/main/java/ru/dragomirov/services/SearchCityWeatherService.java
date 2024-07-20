6c98c980e7c2d67e87d71f627dc1e8420d62026a
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-20 20:17:24 +0300

    feat(019): перенёс логику с сервлета в сервис

--- src/main/java/ru/dragomirov/services/SearchCityWeatherService.java
+++ src/main/java/ru/dragomirov/services/SearchCityWeatherService.java
@@ -1,4 +1,70 @@
 package ru.dragomirov.services;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import ru.dragomirov.dto.request.WeatherByLocationRequestDTO;
+import ru.dragomirov.exception.InvalidParameterException;
+import ru.dragomirov.exception.NotFoundException;
+import ru.dragomirov.exception.api.WeatherApiException;
+import ru.dragomirov.utils.Utils;
+import ru.dragomirov.utils.constants.ApiKeyConstant;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class SearchCityWeatherService {
+    private Utils utils;
+    private final Pattern regex = Pattern.compile("[^a-zA-Z\\s:]");
+
+    public SearchCityWeatherService() {
+        this.utils = new Utils();
+    }
+    public List<WeatherByLocationRequestDTO> getWeatherByCity(String cityName) throws WeatherApiException {
+        validateCityName(cityName);
+        String apiUrl = buildApiUrl(cityName);
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpResponse response = executeRequest(httpClient, apiUrl);
+            return processResponse(response);
+        } catch (IOException e) {
+            throw new WeatherApiException("Error accessing the API");
+        }
+    }
+
+    private void validateCityName(String cityName) {
+        if (cityName.isEmpty()) {
+            throw new InvalidParameterException("Parameter city is invalid");
+        }
+
+        if (regex.matcher(cityName).find()) {
+            throw new InvalidParameterException("City name contains invalid characters");
+        }
+    }
+
+    private String buildApiUrl(String cityName) {
+        String apiKey = ApiKeyConstant.API_KEY_CONSTANT.getValue();
+        return utils.buildCityWeatherApiUrl(cityName, apiKey);
+    }
+
+    private HttpResponse executeRequest(CloseableHttpClient httpClient, String apiUrl) throws IOException {
+        HttpGet request = new HttpGet(apiUrl);
+        return httpClient.execute(request);
+    }
+
+    private List<WeatherByLocationRequestDTO> processResponse(HttpResponse response) throws IOException, WeatherApiException {
+        if (response.getStatusLine().getStatusCode() == 404) {
+            throw new NotFoundException("City was not found");
+        }
+
+        String jsonStr = EntityUtils.toString(response.getEntity());
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        WeatherByLocationRequestDTO[] requestDTOArray = gson.fromJson(jsonStr, WeatherByLocationRequestDTO[].class);
+        return List.of(requestDTOArray);
+    }
 }
--- src/main/java/ru/dragomirov/servlets/SearchCityWeatherServlet.java
+++ src/main/java/ru/dragomirov/servlets/SearchCityWeatherServlet.java
@@ -1,69 +1,32 @@
 package ru.dragomirov.servlets;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.thymeleaf.context.WebContext;
 import ru.dragomirov.config.thymeleaf.TemplateEngineConfig;
 import ru.dragomirov.dto.request.WeatherByLocationRequestDTO;
-import ru.dragomirov.exception.InvalidParameterException;
-import ru.dragomirov.exception.NotFoundException;
 import ru.dragomirov.exception.api.WeatherApiException;
-import ru.dragomirov.utils.Utils;
-import ru.dragomirov.utils.constants.ApiKeyConstant;
+import ru.dragomirov.services.SearchCityWeatherService;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.regex.Pattern;
 
 @WebServlet(name = "SearchCityWeatherServlet", urlPatterns = "/search-city-weather")
 public class SearchCityWeatherServlet extends BaseServlet {
-    private Utils utils;
-    private final Pattern regex = Pattern.compile("[^a-zA-Z\\s:]");
+    private SearchCityWeatherService searchCityWeatherService;
 
     @Override
-    public void init(){
-        this.utils = new Utils();
+    public void init() {
+        this.searchCityWeatherService = new SearchCityWeatherService();
     }
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String cityName = req.getParameter("city");
 
-        if (cityName.isEmpty()) {
-            throw new InvalidParameterException("Parameter city is invalid");
-        }
-
-        if (regex.matcher(cityName).find()) {
-            throw new InvalidParameterException("Is invalid");
-        }
         try {
-            String apiKey = ApiKeyConstant.API_KEY_CONSTANT.getValue();
-            String apiUrl = utils.buildCityWeatherApiUrl(cityName, apiKey);
-
-            CloseableHttpClient httpClient = HttpClients.createDefault();
-            // Создание объекта запроса
-            HttpGet request = new HttpGet(apiUrl);
-            // Выполнение запроса
-            HttpResponse response = httpClient.execute(request);
-
-            if (response.getStatusLine().getStatusCode() == 404) {
-                throw new NotFoundException("City was not found");
-            }
-
-            // Преобразование тела ответа в строку
-            String jsonStr = EntityUtils.toString(response.getEntity());
-            // Десериализация строки JSON в Java-объект
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            WeatherByLocationRequestDTO[] requestDTOArray = gson.fromJson(jsonStr, WeatherByLocationRequestDTO[].class);
-            List<WeatherByLocationRequestDTO> requestDTOList = List.of(requestDTOArray);
+            List<WeatherByLocationRequestDTO> requestDTOList = searchCityWeatherService.getWeatherByCity(cityName);
             WebContext context = TemplateEngineConfig.buildWebContext(req, resp, req.getServletContext());
             context.setVariable("cityList", requestDTOList);
             templateEngine.process("city-weather", context, resp.getWriter());
