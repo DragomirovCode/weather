c6fe71b3ab467a9c93ef93e9802d7b213462c61e
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-25 15:18:33 +0300

    fix(022): удалил лишний код

--- src/main/java/ru/dragomirov/servlet/BackServlet.java
+++ src/main/java/ru/dragomirov/servlet/BackServlet.java
@@ -3,29 +3,16 @@ package ru.dragomirov.servlet;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import ru.dragomirov.dao.HibernateSessionCrudDAO;
-import ru.dragomirov.entity.Session;
-import ru.dragomirov.exception.SessionExpiredException;
 
 import java.io.IOException;
-import java.util.Optional;
 
 @WebServlet(name = "BackServlet", urlPatterns = "/back")
 public class BackServlet extends BaseServlet {
-    private HibernateSessionCrudDAO hibernateSessionCrudDAO;
-
     @Override
-    public void init() {
-        this.hibernateSessionCrudDAO = new HibernateSessionCrudDAO();
-    }
+    public void init() {}
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String otherUuid = (String) getServletContext().getAttribute("myUuid");
-        Optional<Session> session = hibernateSessionCrudDAO.findById(otherUuid);
-        if (session.isEmpty()) {
-            throw new SessionExpiredException("Session has expired");
-        }
         resp.sendRedirect("/");
     }
 }
