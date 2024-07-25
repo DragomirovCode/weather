e90bcee6f669093879dbace59fe5cf3a9744e903
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-25 16:44:14 +0300

    feat: добавил отступы

--- src/main/java/ru/dragomirov/entity/User.java
+++ src/main/java/ru/dragomirov/entity/User.java
@@ -15,8 +15,10 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column(name = "login")
     private String login;
+
     @Column(name = "password")
     private String password;
 
