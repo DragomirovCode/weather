bef9961ce035aaf26f6ac28e5f3e7570b9f141ea
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-27 21:05:12 +0300

    feat(024): добавил аннотацию "DisplayName"

--- src/test/java/service/HibernateSessionFactoryUtilTest.java
+++ src/test/java/service/HibernateSessionFactoryUtilTest.java
@@ -1,6 +1,7 @@
 package service;
 
 import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.dragomirov.util.HibernateSessionFactoryUtil;
 
@@ -8,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class HibernateSessionFactoryUtilTest {
     @Test
+    @DisplayName("it should connect to the database successfully")
     public void getSessionFactory_shouldConnect_inDatabase() {
         SessionFactory sessionFactory = HibernateSessionFactoryUtil.getSessionFactory();
         assertNotNull(sessionFactory);
