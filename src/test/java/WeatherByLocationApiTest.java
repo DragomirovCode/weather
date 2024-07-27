c152b33c190ff11daf3fb571916874b2ad75bce4
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-27 13:27:18 +0300

    feat(023): обновил метод setUp

--- src/test/java/WeatherByLocationApiTest.java
+++ src/test/java/WeatherByLocationApiTest.java
@@ -1,11 +1,19 @@
+import lombok.SneakyThrows;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.dragomirov.dto.request.WeatherByLocationRequestDTO;
-import ru.dragomirov.exception.InvalidParameterException;
+import ru.dragomirov.service.HttpClientService;
 import ru.dragomirov.service.SearchCityWeatherService;
 import ru.dragomirov.util.WeatherApiUrlBuilder;
-import ru.dragomirov.util.constant.ApiKeyConstant;
 
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,18 +22,25 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class WeatherByLocationApiTest {
-    private SearchCityWeatherService service;
-    private WeatherApiUrlBuilder weatherApiUrlBuilder;
+    static final String TEST_JSON_FILE = "src/test/resources/weather_city.json";
+    static final String API_URL = "https://api.openweathermap.org/geo/1.0/direct?q=Kurganinsk&limit=5&appid=";
+    SearchCityWeatherService service;
+    WeatherApiUrlBuilder weatherApiUrlBuilder;
+    HttpClientService httpClientService;
 
+    @SneakyThrows
     @BeforeEach
     void setUp() {
         weatherApiUrlBuilder = mock(WeatherApiUrlBuilder.class);
-        service = new SearchCityWeatherService(weatherApiUrlBuilder);
-    }
+        httpClientService = mock(HttpClientService.class);
+        service = new SearchCityWeatherService(weatherApiUrlBuilder, httpClientService);
 
-    @Test
-    void shouldThrowExceptionForInvalidCityParameter_One() {
-        assertThrows(InvalidParameterException.class, () -> service.getWeatherByCity(""));
+        String jsonWeather = Files.readString(Path.of(TEST_JSON_FILE));
+
+        when(weatherApiUrlBuilder.buildCityWeatherApiUrl(anyString(), anyString())).thenReturn(API_URL);
+
+        HttpResponse mockResponse = createMockResponse(jsonWeather);
+        when(httpClientService.executeRequest(anyString())).thenReturn(mockResponse);
     }
 
     @Test
