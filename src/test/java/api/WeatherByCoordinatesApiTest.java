ee1f62ed97421d82a8542be87254e7be416f12a7
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-27 20:44:56 +0300

    feat(024): обновил метод "shouldReturnWeatherData"

--- src/test/java/api/WeatherByCoordinatesApiTest.java
+++ src/test/java/api/WeatherByCoordinatesApiTest.java
@@ -1,48 +1,80 @@
 package api;
 
+import lombok.SneakyThrows;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import ru.dragomirov.dao.HibernateLocationCrudDAO;
 import ru.dragomirov.dto.request.WeatherByCoordinatesRequestDTO;
 import ru.dragomirov.entity.Location;
+import ru.dragomirov.service.HttpClientService;
 import ru.dragomirov.service.MyLocationsService;
 import ru.dragomirov.util.WeatherApiUrlBuilder;
-import ru.dragomirov.util.constant.ApiKeyConstant;
 
+import java.io.ByteArrayInputStream;
 import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class WeatherByCoordinatesApiTest {
-    private WeatherApiUrlBuilder weatherApiUrlBuilder;
-    private MyLocationsService service;
+    static final String TEST_JSON_FILE = "src/test/resources/weather_forecast_by_coordinates.json";
+    static final String API_URL = "https://api.openweathermap.org/data/2.5/weather?lat=44.8851635&lon=40.5894674&appid=&units=metric";
+    WeatherApiUrlBuilder weatherApiUrlBuilder;
+    HttpClientService httpClientService;
+    MyLocationsService service;
+    HibernateLocationCrudDAO hibernateLocationCrudDAO;
 
+    @SneakyThrows
     @BeforeEach
     void setUp() {
+        hibernateLocationCrudDAO = new HibernateLocationCrudDAO();
+
+        httpClientService = mock(HttpClientService.class);
         weatherApiUrlBuilder = mock(WeatherApiUrlBuilder.class);
-        service = new MyLocationsService(weatherApiUrlBuilder);
+        service = new MyLocationsService(weatherApiUrlBuilder, httpClientService);
+
+        String jsonWeather = Files.readString(Path.of(TEST_JSON_FILE));
+
+        when(weatherApiUrlBuilder.buildLatLonCityWeatherApiUrl(any(BigDecimal.class), any(BigDecimal.class), any(String.class)))
+                .thenReturn(API_URL);
+
+        HttpResponse mockResponse = createMockResponse(jsonWeather);
+        when(httpClientService.executeRequest(anyString())).thenReturn(mockResponse);
     }
 
     @Test
-    void shouldReturnWeatherData() {
+    @DisplayName("should return weather data about temperature actual")
+    void getWeatherDataForLocations_shouldWeatherDataAboutTemperatureActual_inApi() {
         Location location = new Location();
-        location.setId(1);
-        location.setLatitude(BigDecimal.valueOf(55.7504461));
-        location.setLongitude(BigDecimal.valueOf(37.6174943));
-        location.setUserId(1);
-
-        String expectedUrl = "https://api.openweathermap.org/data/2.5/weather?lat=55.7504461&lon=37.6174943&appid="
-                + ApiKeyConstant.API_KEY_CONSTANT.getValue() + "&units=metric";
-
-        when(weatherApiUrlBuilder.buildLatLonCityWeatherApiUrl(any(BigDecimal.class), any(BigDecimal.class), any(String.class)))
-                .thenReturn(expectedUrl);
+        location.setLatitude(new BigDecimal("44.8851635"));
+        location.setLongitude(new BigDecimal("40.5894674"));
+        hibernateLocationCrudDAO.create(location);
 
         List<WeatherByCoordinatesRequestDTO> locationWeatherData = service.getWeatherDataForLocations(Collections.singletonList(location));
+
+        assertNotNull(locationWeatherData);
+        assertFalse(locationWeatherData.isEmpty());
+        assertEquals(BigDecimal.valueOf(25), locationWeatherData.get(0).main.temperatureActual);
+    }
+
+    @SneakyThrows
+    HttpResponse createMockResponse(String json) {
+        // add mock
+        HttpResponse mockResponse = mock(HttpResponse.class);
+        StatusLine mockStatusLine = mock(StatusLine.class);
+        HttpEntity mockEntity = mock(HttpEntity.class);
 
         // Setting up the behavior for StatusLine
         when(mockStatusLine.getStatusCode()).thenReturn(200);
