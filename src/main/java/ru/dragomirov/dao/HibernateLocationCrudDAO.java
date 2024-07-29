11b8a5aa0368279389448c582f05b85309d23379
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-29 17:11:55 +0300

    feat(028): добавил обработку ошибок

--- src/main/java/ru/dragomirov/dao/HibernateLocationCrudDAO.java
+++ src/main/java/ru/dragomirov/dao/HibernateLocationCrudDAO.java
@@ -1,6 +1,7 @@
 package ru.dragomirov.dao;
 
 import ru.dragomirov.entity.Location;
+import ru.dragomirov.exception.SaveLocationException;
 import ru.dragomirov.util.HibernateSessionManagerUtil;
 
 import java.math.BigDecimal;
@@ -10,9 +11,13 @@ import java.util.Optional;
 public class HibernateLocationCrudDAO implements LocationDAO {
     @Override
     public void create(Location entity) {
-        HibernateSessionManagerUtil.performTransaction(session ->
-                        session.save(entity),
-                "Произошла ошибка при выполнении метода 'create'(HibernateLocationCrudDAO)");
+        try {
+            HibernateSessionManagerUtil.performTransaction(session ->
+                            session.save(entity),
+                    "Произошла ошибка при выполнении метода 'create'(HibernateLocationCrudDAO)");
+        } catch (Exception e) {
+            throw new SaveLocationException("Such a location already exists in the database");
+        }
     }
 
     @Override
