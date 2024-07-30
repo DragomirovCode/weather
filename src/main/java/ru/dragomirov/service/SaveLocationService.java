dbf695d8ef71dd84a86273737b2280445a662a6e
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-07-31 01:30:44 +0300

    chore(031): убрал проверку на уникальность

--- src/main/java/ru/dragomirov/service/SaveLocationService.java
+++ src/main/java/ru/dragomirov/service/SaveLocationService.java
@@ -4,11 +4,9 @@ import ru.dragomirov.dao.HibernateLocationCrudDAO;
 import ru.dragomirov.dao.HibernateSessionCrudDAO;
 import ru.dragomirov.entity.Location;
 import ru.dragomirov.entity.Session;
-import ru.dragomirov.exception.SaveLocationException;
 import ru.dragomirov.exception.SessionExpiredException;
 
 import java.math.BigDecimal;
-import java.util.Optional;
 
 public class SaveLocationService {
     private HibernateLocationCrudDAO hibernateLocationCrudDAO;
@@ -21,15 +19,8 @@ public class SaveLocationService {
 
     public void saveLocation(String uuid, String city, String latitudeStr, String longitudeStr) {
         Session session = validateSession(uuid);
-        Optional<Location> location = hibernateLocationCrudDAO.findByLocationLatitudeAndLongitudeAndUserId(
-                new BigDecimal(latitudeStr), new BigDecimal(longitudeStr), session.getUserId(), city);
-
-        if (location.isEmpty()) {
-            Location newLocation = new Location(city, new BigDecimal(latitudeStr), new BigDecimal(longitudeStr), session.getUserId());
-            hibernateLocationCrudDAO.create(newLocation);
-        } else {
-            throw new SaveLocationException("Such a location already exists in the database");
-        }
+        Location newLocation = new Location(city, new BigDecimal(latitudeStr), new BigDecimal(longitudeStr), session.getUserId());
+        hibernateLocationCrudDAO.create(newLocation);
     }
 
     private Session validateSession(String uuid) {
